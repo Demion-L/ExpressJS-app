283a5517352a1565780798db44159a756a1ad218
Dmitriy Latysh <pradovds@ukr.net>
2021-04-14 15:23:55 +0300

    first commit

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
 import express from "express";
+import path from "path";
+import { requestTime, logger } from "./middlewares.js";
 
 const app = express();
 // const PORT = process.env.PORT ?? 3000
 const PORT = 3000;
+const __dirname = path.resolve();
 
-app.get('/', (req, res) => {
-   
-})
+app.set("view engine", "ejs");
+app.set("views", path.resolve(__dirname, "ejs"));
+
+app.use(express.static(path.resolve(__dirname, "static")));
+app.use(requestTime);
+// app.use(logger);
+
+app.get("/", (req, res) => {
+  res.render("index", {title: 'Main Page', active: 'main'});
+});
+
+app.get("/features", (req, res) => {
+  res.render("features", {title: 'Features Page', active: 'features'});
+});
+
+// app.get("/", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "static", "index.html"));
+// });
+
+// app.get("/download", (req, res) => {
+//   console.log(req.requestTime);
+
+//   res.download(path.resolve(__dirname, "static", "index.html"));
+// });
 
 app.listen(PORT, () => {
   console.log(`Server has been started on port ${PORT}!`);
